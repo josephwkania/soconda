cbc92d5095713b7df2290c2ce01da59995335a40
josephwkania <60994292+josephwkania@users.noreply.github.com>
2024-11-26 11:46:46 +0000

    Initial commit of Universe Scripts.

---
+++ deploy/install_universe.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#========== Configuration ==================
+
+# Location for conda base
+base_dir=/opt/soconda/base
+
+# Location for temp clones
+temp_dir=/root/soconda/deploy
+
+# Location for installs
+install_dir=/opt/soconda/envs/
+
+# Module file directory
+module_dir=/opt/apps/modulefiles/Core
+
+#===========================================
+
+version=$1
+if [ "x${version}" = "x" ]; then
+    echo "usage:  $0 <soconda branch/tag/hash>"
+    exit 1
+fi
+
+# Location for clone repos
+mkdir -p "${temp_dir}"
+today=$(date +%Y%m%d)
+
+# The name of env
+env_name="${install_dir}/soconda"
+
+# The full version
+env_version="${today}_${version}"
+
+# Clone location
+clone_dir="${temp_dir}/${env_version}"
+
+# Make sure the module dir exists
+mkdir -p "${module_dir}"
+
+# Log file
+logfile="${temp_dir}/log_${env_version}"
+echo "Starting at $(date)" > "${logfile}"
+
+# Get specified commit
+echo "Making shallow clone of ${version} in ${clone_dir}" >> "${logfile}"
+
+if [ -d "${clone_dir}" ]; then
+    rm -rf "${clone_dir}"
+fi
+
+git clone --depth=1 --single-branch --branch=${version} https://github.com/simonsobs/soconda.git "${clone_dir}" >> "${logfile}" 2>&1
+
+# Activate the base environment and keep it up to date.
+source "${base_dir}/etc/profile.d/conda.sh"
+conda activate base >> "${logfile}" 2>&1
+conda update --yes -n base conda conda-build >> "${logfile}" 2>&1
+conda update --yes --all -n base >> "${logfile}" 2>&1
+
+# Build things from the temp directory
+
+pushd "${clone_dir}" >/dev/null 2>&1
+mkdir -p "build"
+pushd "build" >/dev/null 2>&1
+
+export MPICC="cc"
+
+eval "${clone_dir}/soconda.sh" \
+    -c "perlmutter" \
+    -e "${env_name}" \
+    -v "${env_version}" \
+    -m "${module_dir}" >> "${logfile}" 2>&1
+
+popd >/dev/null 2>&1
+popd >/dev/null 2>&1
+
+# Update permissions
+chmod -R g-w,g+rX "${env_name}_${env_version}" >> "${logfile}" 2>&1
+chmod -R g-w,g+rX "${module_dir}/soconda" >> "${logfile}" 2>&1
+
+# Return name of log file
+echo "${logfile}"
---
+++ deploy/install_universe_tag.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+#========== Configuration ==================
+
+# Location for persistent soconda checkout
+git_dir=${HOME}/soconda
+
+# Location of installed envs
+install_dir=/opt/soconda
+
+# Log dir- make a temp dir in the git checkout
+log_dir="${git_dir}/logs"
+
+# Module file directory
+# module_dir=/opt/apps/modulefiles/Core
+module_dir=/usr/share/Modules/modulefiles
+
+# Typical size of an environment, in GB
+typical=6
+
+#===========================================
+
+# Function to check available space in software
+# get_common_free_gb () {
+#     cmn_used=$(prjquota --cmn sobs | grep sobs | awk '{print $2}')
+#     cmn_total=$(prjquota --cmn sobs | grep sobs | awk '{print $3}')
+#     cmn_remain=$(( ${cmn_total} - ${cmn_used} ))
+#     echo ${cmn_remain}
+# }
+
+# Check that NERSC_HOST is set
+host=universe
+# if [ "x${host}" = "x" ]; then
+#     echo "This script only runs at NERSC"
+#     exit 1
+# fi
+
+# If the latest tag is already installed, we do not need to make an
+# extra log in the log dir.  We already have a slurm/scron log that is
+# always created.
+
+# Update the persistent git checkout and find the most recent tag
+pushd "${git_dir}" >/dev/null 2>&1
+git checkout main
+git fetch --tags
+git rebase origin/main
+git remote prune origin
+latest=$(git describe --tags $(git rev-list --tags --max-count=1))
+popd >/dev/null 2>&1
+
+# See if we already have this tag installed
+found=no
+found_date=""
+for item in $(ls ${install_dir}); do
+    check=$(echo ${item} | sed -e "s/soconda_.*_\(.*\)/\1/")
+    found_date=$(echo ${item} | sed -e "s/soconda_\(.*\)_.*/\1/")
+    if [ "${check}" = "${latest}" ]; then
+        found=yes
+    fi
+done
+
+if [ "${found}" = "yes" ]; then
+    # Nothing to do
+    echo "Latest tag '${latest}' was already installed on ${found_date}"
+    exit 0
+fi
+
+# If we got this far, we need to install things.
+
+# Make sure log dir exists
+mkdir -p "${log_dir}"
+
+now=$(date +%Y%m%d-%H%M%S)
+today=$(date +%Y%m%d)
+log_file="${log_dir}/check_tags_${host}_${now}"
+
+# Create the log file
+echo "Starting at ${now}" > "${log_file}"
+
+# Get the module file version that will be installed
+mod_ver="${today}_${latest}"
+
+send_log=no
+annoy="${git_dir}/.already_annoyed"
+remain=10 # $(get_common_free_gb)
+if (( remain < typical )); then
+    echo "Only ${remain} GB are available in /global/common/software/sobs" >> "${log_file}"
+    echo "Installing latest tag requires approximately ${typical} GB" >> "${log_file}"
+    echo "SKIPPING until disk space is cleared." >> "${log_file}"
+    if [ ! -e "${annoy}" ]; then
+        send_log=yes
+        touch "${annoy}"
+    fi
+else
+    send_log=yes
+    rm -f "${git_dir}/.already_annoyed"
+    echo "Latest tag '${latest}' not found, installing..." >> "${log_file}"
+    echo "Note: ${remain} GB are available in /global/common/software/sobs" >> "${log_file}"
+    echo "Installing latest tag requires approximately ${typical} GB" >> "${log_file}"
+    install_log=$(eval "${git_dir}/deploy/install_${host}.sh" "${latest}")
+    if [ -f "${install_log}" ]; then
+        # There were no errors, and the log file was returned
+        cat "${install_log}" >> "${log_file}"
+    else
+        # The script must have printed out some errors
+        echo "${install_log}" >> "${log_file}"
+    fi
+fi
+
+# Only update the "stable" symlink if the build completed and the modulefile
+# was generated.
+mod_dir="${module_dir}/soconda"
+mod_latest="${mod_dir}/${mod_ver}.lua"
+if [ -e "${mod_latest}" ]; then
+    rm -f "${mod_dir}/stable.lua" \
+    && ln -s "${mod_latest}" "${mod_dir}/stable.lua"
+else
+    echo "ERROR:  module file ${mod_latest} was not created- leaving stable symlink" >> "${log_file}" 
+fi
+
+echo "Finished installing tag '${latest}' on host ${NERSC_HOST} at $(date +%Y%m%d-%H%M%S)" >> "${log_file}"
+
+if [ "${send_log}" = "yes" ]; then
+    # Get our webhook address from the environment
+    slack_web_hook=${SLACKBOT_SOCONDA}
+
+    if [ "x${slack_web_hook}" = "x" ]; then
+        echo "Environment variable SLACKBOT_SOCONDA not set- skipping notifications" >> "${log_file}"
+    else
+        # Create the JSON payload.
+        slackjson="${log_file}_slack.json"
+        headtail=12
+        echo -e "{\"text\":\"soconda install tag (log at \`${log_file}\`):\n\`\`\`$(head -n ${headtail} ${log_file} | sed -e "s|'|\\\'|g")\`\`\`\n(Snip)\n\`\`\`$(tail -n ${headtail} ${log_file} | sed -e "s|'|\\\'|g")\`\`\`\"}" > "${slackjson}"
+        # Post it.
+        slackerror=$(curl -X POST -H 'Content-type: application/json' --data "$(cat ${slackjson})" ${slack_web_hook})
+        echo "Slack API post  ${slackerror}" >> "${log_file}"
+    fi
+fi
